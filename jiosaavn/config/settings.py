581d186f360cfa09e8806eba9fb5c237ee369a9a
Labani Behera <101978891+ZxLbni@users.noreply.github.com>
2024-11-13 09:40:42 +0530

    Update settings.py

--- jiosaavn/config/settings.py
+++ jiosaavn/config/settings.py
@@ -1,8 +1,8 @@
 from os import getenv
 
-API_ID = getenv("API_ID")
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = getenv("API_ID", 22419004)
+API_HASH = getenv("API_HASH", "34982b52c4a83c2af3ce8f4fe12fe4e1")
+BOT_TOKEN = getenv("BOT_TOKEN", "7564755990:AAH30wsTjb6ZjhLA4v_ddfoNiRaWv8U2KMk")
 BOT_COMMANDS = (
     ("start", "Initialize the bot and check its status"),
     ("settings", "Configure and manage bot settings"),
