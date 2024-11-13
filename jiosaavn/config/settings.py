e5062480705ba35b076f938c5fa4dc2da2fb1482
Labani Behera <101978891+ZxLbni@users.noreply.github.com>
2024-11-13 09:44:57 +0530

    Update settings.py

--- jiosaavn/config/settings.py
+++ jiosaavn/config/settings.py
@@ -1,8 +1,11 @@
 from os import getenv
 
-API_ID = getenv("API_ID", 22419004)
+# Environment configurations with default values
+API_ID = int(getenv("API_ID", "22419004"))  # Ensure API_ID is an integer
 API_HASH = getenv("API_HASH", "34982b52c4a83c2af3ce8f4fe12fe4e1")
 BOT_TOKEN = getenv("BOT_TOKEN", "7564755990:AAH30wsTjb6ZjhLA4v_ddfoNiRaWv8U2KMk")
+
+# Bot commands configuration
 BOT_COMMANDS = (
     ("start", "Initialize the bot and check its status"),
     ("settings", "Configure and manage bot settings"),
@@ -10,6 +13,7 @@ BOT_COMMANDS = (
     ("about", "Learn more about the bot and its features"),
 )
 
+# Database configuration, host, and port settings
 DATABASE_URL = getenv("DATABASE_URL", None)
 HOST = getenv("HOST", "0.0.0.0")
-PORT = int(getenv("PORT", "8080"))
+PORT = int(getenv("PORT", "8080"))  # Convert PORT to an integer
