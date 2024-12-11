4b0cd8acd8d20685dec2c78a74adeb3c9af98305
zxxlbni <noobmethod9@gmail.com>
2024-12-11 15:13:43 +0530

    Update settings.py

--- jiosaavn/config/settings.py
+++ jiosaavn/config/settings.py
@@ -3,7 +3,7 @@ from os import getenv
 # Environment configurations with default values
 API_ID = int(getenv("API_ID", "22419004"))  # Ensure API_ID is an integer
 API_HASH = getenv("API_HASH", "34982b52c4a83c2af3ce8f4fe12fe4e1")
-BOT_TOKEN = getenv("BOT_TOKEN", "7564755990:AAGovA__Fo-jw3v4X0cpcyLmsNre3OAKFcA")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 # Bot commands configuration
 BOT_COMMANDS = (
@@ -16,4 +16,4 @@ BOT_COMMANDS = (
 # Database configuration, host, and port settings
 DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://mrnoobx:DAZCdTczVWyECi04@cluster0.sedgwxy.mongodb.net/?retryWrites=true&w=majority")
 HOST = getenv("HOST", "")
-PORT = int(getenv("PORT", "8484"))  # Convert PORT to an integer
+PORT = int(getenv("PORT", "8080"))  # Convert PORT to an integer
